2f820cf4b1d8b5e3be8a007b7e3cbf9af6788af2
Elffet <masahiro.sakai.ms@gmail.com>
2020-06-28 14:41:37 +0900

    output-areaコンポーネント作成

--- src/angular-conversion-app/src/app/app.module.ts
+++ src/angular-conversion-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { InputAreaComponent } from './input-area/input-area.component';
 import { ConversionButtonComponent } from './conversion-button/conversion-button.component';
+import { OutputAreaComponent } from './output-area/output-area.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputAreaComponent,
-    ConversionButtonComponent
+    ConversionButtonComponent,
+    OutputAreaComponent
   ],
   imports: [
     BrowserModule
